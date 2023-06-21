76a065ff6d25d944191d4787da63233a5359d869
Abinash Mainali <mainali.abinash@gmail.com>
2023-06-21 14:26:38 +1000

    Divide app into header main and footer for clear web design

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,48 @@
-
 import './App.css';
-import {Link, Route, Routes} from "react-router-dom";
+import {NavLink, Route, Routes} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Navbar} from "react-bootstrap";
+import {Container, Nav, Navbar} from "react-bootstrap";
 import {Home} from "./pages/Home";
 import {Project} from "./pages/Project";
+
 function App() {
-  return (
-    <div className="App">
-        <Navbar className="navbar-expand-md navbar-dark bg-dark">
-            <Navbar.Toggle aria-controls={"basic-navbar-nav"} />
-            <Navbar.Collapse id={"basic-navbar-nav"}>
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <Link to={"/"} className="nav-link">Home</Link>
-                    </li>
-                    <li>
-                        <Link to={"/project"} className="nav-link">Project</Link>
-                    </li>
-                </ul>
-            </Navbar.Collapse>
-        </Navbar>
-        <div className="container flex-row">
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/project" element={<Project />} />
+    return (
+        <div className={"App"}>
+            <Navbar expand={"md"} bg={"dark"} variant={"dark"} className={"navbar-light"}>
+                <Container>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <NavLink to="/" className="nav-link">
+                                Home
+                            </NavLink>
+                            <NavLink to="/project" className="nav-link">
+                                Project
+                            </NavLink>
 
-            </Routes>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <div className="App-main">
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/project" element={<Project/>}/>
+
+                </Routes>
+
+            </div>
+            <div className={"App-footer"}>
+                <Container>
+                    <p>© 2021 Abinash Mainali</p>
+                </Container>
 
 
+            </div>
 
+        </div>
 
-
-
-
-
-
-    </div>
-    </div>
-
-  );
+    );
 }
 
 export default App;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,47 @@
-export function Home(){
-    return(
+import {Container, Figure} from "react-bootstrap";
+
+export function Home() {
+
+    return (
         <div>
-            <h1>Home</h1>
+            <Container className={"Home"}>
+                <div className={"Home-header m-2"}>
+                <h1>Hi! I am Abinash Mainali</h1>
+                </div>
+
+                <Figure className={"Home"}>
+                    <Figure.Image
+                        width={171}
+                        height={180}
+                        alt="171x180"
+                        src={"https://avatars.githubusercontent.com/u/27627798?s=400&u=40dbf5a57c35c3036dabf067e0c429ca86f80c23&v=4"}
+                    />
+
+
+                </Figure>
+                <h2>Graduate Software Developer</h2>
+                <p> I am a graduate software developer with a year of experience in developing web applications using
+                    React, Node, and PHP.
+                    I have a Bachelor's degree in Information Technology from the University of Sunshine Coast. </p>
+                <div className={"web-link"}>
+                    <a href="https://www.linkedin.com/in/abinash-mainali-1b1b3a1b0/" className={"m-2 d-inline-block"}>
+                        <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="linkedin" width="50"
+                             height="50"/>
+                    </a>
+                    <a href="https://github.com/AbinashMainali" className={"m-2 d-inline-block"}>
+                        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github" width="50"
+                             height="50"/>
+                    </a>
+                    <a href="https://www.instagram.com/abinashmainali/" className={"m-2 d-inline-block"}>
+                        <img src="https://cdn-icons-png.flaticon.com/512/174/174855.png" alt="instagram" width="50"
+                                height="50"/>
+                    </a>
+                </div>
+
+
+            </Container>
         </div>
+
+
     )
 }
